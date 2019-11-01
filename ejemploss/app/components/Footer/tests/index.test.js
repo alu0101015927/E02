e546fe12f4afd29329681f3a031d44916d73e199
Vlad <vladcomanescu@yahoo.com>
2019-11-01 20:40:54 +0000

    rename folder ejemploss

--- ejemploss/app/components/Footer/tests/index.test.js
+++
@@ -1,8 +0,0 @@
-/* import React from 'react';
-import renderer from 'react-test-renderer';
-import { IntlProvider } from 'react-intl';
-import { Provider } from 'react-redux';
-import { browserHistory } from 'react-router-dom';
-
-import Footer from '../index';
-*/
