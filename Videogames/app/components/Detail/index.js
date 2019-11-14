9c48c1b6f78b3fad5b534dbdc1ec97372165bf00
Marta García <alu0101015927@ull.edu.es>
2019-11-14 02:53:40 +0000

    modificando el componente detail

--- Videogames/app/components/Detail/index.js
+++ Videogames/app/components/Detail/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import { Container, CircularProgress, Typography} from '@material-ui/core';
 
 function Detail() {
   return (
+    <Container>
+      <Typography variant="h2"> Titulo del videojuego </Typography>
+      <CircularProgress size={100} color="primary"/> <img alt="caraturala del videojuego">
+      <Typography>Aqui va un texto descriptivo del videojuego a modo de sinopsis de la historia o resumen para llamar la atención y venderlo, es decir, con la indención de generar interes por parte del usuario en comprar este juego</Typography>
+      <Typography><strong>Consolas disponibles:</strong><Typography>
+      <Typography><strong>Precio: </strong> x€<Typography>
+    </Container>
   );
+
 }
 
 export default Detail;
