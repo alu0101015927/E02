41e83d005af266f6cf6ec32a40f10cc16ffae69e
Alejandro Gonzalez Gonzalez <alu0101015729@ull.edu.es>
2020-04-28 14:17:43 +0100

    style

--- Xgames/Frontend/src/components/sections/HomeView/HomeView.tsx
+++ Xgames/Frontend/src/components/sections/HomeView/HomeView.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import Header from '../../Header/Header';
 import Slider from '../../Slider/Slider';
 import Sidebar from '../../SideBar/SideBar';
@@ -12,13 +12,39 @@ const HomeView: React.FC = () => {
       <Header />
       <Slider />
       <Grid container alignContent='center' alignItems='flex-start' spacing={3} >
+        <Grid item xs={12} sm={12} md={9} xl={9} lg={9}>
+          <Grid container alignContent='flex-start' justify='flex-start' alignItems='flex-start' spacing={3} >
+            <Grid item xs={12} sm={12} md={12} xl={12} lg={12} justify='center'>
+              <h1>Titulos mas buscados</h1>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} xl={6} lg={6} spacing={1} justify='flex-start'>
+              <Card />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} xl={6} lg={6} spacing={1} justify='flex-start'>
+              <Card />
+            </Grid>
+          </Grid>
+        </Grid>
         <Grid item xs={12} sm={12} md={3} xl={3} lg={3} justify='flex-start'>
           <Sidebar />
         </Grid>
         <Grid item xs={12} sm={12} md={9} xl={9} lg={9}>
           <Grid container alignContent='flex-start' justify='flex-start' alignItems='flex-start' spacing={3} >
-          <Grid item xs={12} sm={12} md={12} xl={12} lg={12} justify='center'>
-            <h1>Titulos mas buscados</h1>
+            <Grid item xs={12} sm={12} md={12} xl={12} lg={12} justify='center'>
+              <h1>Favoritos</h1>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} xl={6} lg={6} justify='flex-start'>
+              <Card />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} xl={6} lg={6} justify='flex-start'>
+              <Card />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sm={12} md={9} xl={9} lg={9}>
+          <Grid container alignContent='flex-start' justify='flex-start' alignItems='flex-start' spacing={3} >
+            <Grid item xs={12} sm={12} md={12} xl={12} lg={12} justify='center'>
+              <h1>Favoritos</h1>
             </Grid>
             <Grid item xs={12} sm={12} md={6} xl={6} lg={6} justify='flex-start'>
               <Card />
@@ -29,10 +55,6 @@ const HomeView: React.FC = () => {
           </Grid>
         </Grid>
       </Grid>
-      <h1>Relativos</h1>
-      <Card />
-      <h1>Favoritos</h1>
-      <Card />
       <Footer />
     </div>
   );
